1b8622319dda101a402a2945e7b052ea632d8f7b
sun-yryr <taittide@gmail.com>
2018-11-02 22:25:55 +0900

    [fin] 1-7 極座標変換まで

--- dsp/1-7/kadai.c
+++ dsp/1-7/kadai.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef struct dee {
+typedef struct {
     double re;
     double im;
 } Complex;
+
+typedef struct {
+    double value;
+    double theta;
+} Polar;
 
 //プロトタイプ宣言
 Complex addition(Complex, Complex);
 Complex subtraction(Complex, Complex);
 Complex multiplication(Complex, Complex);
 Complex division(Complex, Complex);
+void twid(Complex*, int);
+Complex conjugate(Complex);
+double absolute(Complex);
+double absolute2(Complex);
+Polar polarTrans(Complex);
+Complex complexTrans(Polar);
 
 int main() {
     printf("Hello World!\n");
+    Complex test;
+    test.im = 3.5;
+    test.re = 2;
+    printf("Complex: %lf : %lf\n", test.re, test.im);
+    Polar test2 = polarTrans(test);
+    printf("Polar: %lf : %lf\n", test2.value, test2.theta);
+    test = complexTrans(test2);
+    printf("Complex: %lf : %lf\n", test.re, test.im);
     return 0;
 }
 
@@ -33,10 +52,62 @@ Complex subtraction(Complex A, Complex B) {
 
 Complex multiplication(Complex A, Complex B) {
     Complex returnData;
+    double a = A.re,
+        b = A.im,
+        c = B.re,
+        d = B.im;
+    returnData.re = (a * c) - (b * d);
+    returnData.im = (b * c) + (a * d);
     return returnData;
 }
 
 Complex division(Complex A, Complex B) {
     Complex returnData;
+    double a = A.re,
+        b = A.im,
+        c = B.re,
+        d = B.im;
+    double nnn = (c * c) + (d * d);
+    returnData.re = ((a * c) + (b * d)) / nnn;
+    returnData.im = ((b * c) - (a * d)) / nnn;
+    return returnData;
+}
+
+void twid(Complex *A, int N) {
+    for(int i=0; i<N; i++) {
+        double theta = ((2 * M_PI) / N) * i;
+        A[i].re = cos(theta);
+        A[i].im = sin(theta);
+    }
+}
+
+Complex conjugate(Complex A) {
+    Complex returnData;
+    returnData.re = A.re;
+    returnData.im = -1 * A.im;
+    return returnData;
+}
+
+double absolute(Complex A) {
+    //return sqrt((A.re * A.re) + (A.im * A.im));
+    return sqrt(absolute2(A));
+}
+
+double absolute2(Complex A) {
+    Complex returnData = multiplication(A, conjugate(A));
+    return returnData.re;
+}
+
+Polar polarTrans(Complex A) {
+    Polar returnData;
+    returnData.value = absolute(A);
+    returnData.theta = atan(A.im / A.re);
+    return returnData;
+}
+
+Complex complexTrans(Polar A) {
+    Complex returnData;
+    returnData.re = A.value * cos(A.theta);
+    returnData.im = A.value * sin(A.theta);
     return returnData;
 }
